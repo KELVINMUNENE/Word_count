abf72a9835da8c8c2932017a7c0b5e542b16d354
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-30 14:01:29 +0300

    added a method to be able to call a specific word from the array of words

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,1 +1,12 @@
-
+class String
+  define_method(:word_count) do |word_to_count|
+    count = 0
+    count = self.split()
+    count.split(" ").each() do |text_word|
+      if text_word == word_to_count
+        count += 1
+      end
+    end
+    count
+  end
+end
