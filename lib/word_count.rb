8ee752afafa8470d308e20dba87169e3d60e1969
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-30 14:22:34 +0300

    described my method in the spec file

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,12 +1,12 @@
 class String
-  define_method(:word_count) do |word_to_count|
-    count = 0
-    count = self.split()
-    count.split(" ").each() do |text_word|
-      if text_word == word_to_count
-        count += 1
+  define_method(:count) do |word_to_count|
+    counter = 0
+    split = self.split()
+    split.each() do |word|
+      if word==word_to_count
+        counter+=1
       end
     end
-    count
+    counter
   end
 end
