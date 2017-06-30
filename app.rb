f457334439a606d824567e3dc6c132281b19950e
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-06-30 14:37:31 +0300

    added a place holder + changed my form file name

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
-require('sinatra')
-  require('sinatra/reloader')
+    require('sinatra')
+    require('sinatra/reloader')
+    require('descriptive_statistics')
     require('./lib/word_count')
-    also_reload('lib/word_count.rb')
+    also_reload('lib/**/*.rb')
 
-    get('/form') do
-       erb(:form)
+    get('/') do
+      erb(:index)
+    end
+
+    get('/result') do
+      word = params.fetch(('word').to_s)
+      @word = params.fetch(('word').to_s)
+      @result = params.fetch('sentence').to_s.count(word)
+      erb(:result)
     end
